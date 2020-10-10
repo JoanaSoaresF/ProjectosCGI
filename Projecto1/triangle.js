047491589b56bc3d53791e0ee7ea7323af70484d
Goncalo Lourenco <gm.lourenco@gmail.com>
2020-10-10 20:41:27 +0100

    Aparece a grelha preta mas desapareceram as funcoes

--- Projecto1/triangle.js
+++ Projecto1/triangle.js
@@ -1,10 +1,28 @@
 var gl;
+var gridProgram;
 var program;
+var gridBufferId;
 var bufferId;
+var grid = [];
 var vertices = [];
 var funcLoc;
 var vTimeSample;
+var colorLoc;
+var vPosition;
 
+const NUM_COLS = 8;
+const NUM_LINES = 12;
+
+function grid_vertices(){
+    for (let i = 0; i < NUM_COLS - 1; i++) {
+        grid.push(vec2(i * 2 / (NUM_COLS - 1) - 1, -1));
+        grid.push(vec2(i * 2 / (NUM_COLS - 1) - 1, 1));
+    }
+    for (let i = 0; i < NUM_LINES - 1; i++) {
+        grid.push(vec2(-1, i * 2 / (NUM_LINES - 1) - 1));
+        grid.push(vec2(1, i * 2 / (NUM_LINES - 1) - 1));
+    }
+}
 
 function formVertex() {
     for (let i = 0.0; i < 9999.0; i = i + 1.0) {
@@ -20,17 +38,25 @@ window.onload = function init() {
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) { alert("WebGL isn't available"); }
 
+    grid_vertices();
     formVertex();
 
     // Configure WebGL
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Load shaders and initialize attribute buffers
+    gridProgram = initShaders(gl, "gridVertex-shader", "gridFragment-shader");
     program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    //gl.useProgram(program);
 
     // Load the data into the GPU
+    gridBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(grid), gl.STATIC_DRAW);
+
+    vPosition = gl.getAttribLocation(gridProgram, "vPosition");
+
     bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
@@ -43,8 +69,27 @@ window.onload = function init() {
 }
 
 function render() {
+
+    //gl.useProgram(program);
     computeFunction();
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+    gl.useProgram(gridProgram);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gridBufferId);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    for(let i = 0; i < NUM_COLS-1; i++){
+        gl.drawArrays(gl.LINES, 2*i, 2);
+    } 
+    for (let i = 0; i < NUM_LINES - 1; i++) {
+        gl.drawArrays(gl.LINES, 2 * (NUM_COLS + i), 2);
+    }
+
+
+    gl.useProgram(program);
 
     gl.vertexAttribPointer(vTimeSample, 1, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vTimeSample);
