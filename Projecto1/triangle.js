a5e0bbdd1b72150ad7f113aa85104e32b6c22d1f
JoanaSFaria <js.faria@campus.fct.unl.pt>
2020-10-11 16:38:50 +0100

    Funções que se mexem

--- Projecto1/triangle.js
+++ Projecto1/triangle.js
@@ -1,3 +1,6 @@
+
+//Variables
+
 var gl;
 var gridProgram;
 var program;
@@ -9,11 +12,17 @@ var funcLoc;
 var vTimeSample;
 var colorLoc;
 var vPosition;
+var voltScaleLoc;
+var timeScaleLoc;
+var amplitudeLoc;
+var timeLoc;
+var time = 0;
 
+//Constants
 const NUM_COLS = 8;
 const NUM_LINES = 12;
 
-function grid_vertices(){
+function grid_vertices() {
     for (let i = 0; i < NUM_COLS - 1; i++) {
         grid.push(vec2(i * 2 / (NUM_COLS - 1) - 1, -1));
         grid.push(vec2(i * 2 / (NUM_COLS - 1) - 1, 1));
@@ -28,9 +37,6 @@ function formVertex() {
     for (let i = 0.0; i < 9999.0; i = i + 1.0) {
         vertices.push(i);
     }
-}
-function computeFunction() {
-    gl.uniform1i(funcLoc, document.getElementById("function").value);
 }
 
 window.onload = function init() {
@@ -64,37 +70,56 @@ window.onload = function init() {
     // Associate our shader variables with our data buffer
     vTimeSample = gl.getAttribLocation(program, "vTimeSample");
     funcLoc = gl.getUniformLocation(program, "func");
+    voltScaleLoc = gl.getUniformLocation(program, "voltScale");
+    timeScaleLoc = gl.getUniformLocation(program, "timeScale");
+    amplitudeLoc = gl.getUniformLocation(program, "amplitude");
+    timeLoc = gl.getUniformLocation(program, "time");
+
+
 
     render();
 }
-
-function render() {
-
-    //gl.useProgram(program);
-    computeFunction();
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-
+function drawGrid() {
     gl.useProgram(gridProgram);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, gridBufferId);
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    for(let i = 0; i < NUM_COLS-1; i++){
-        gl.drawArrays(gl.LINES, 2*i, 2);
-    } 
+    for (let i = 0; i < NUM_COLS - 1; i++) {
+        gl.drawArrays(gl.LINES, 2 * i, 2);
+    }
     for (let i = 0; i < NUM_LINES - 1; i++) {
         gl.drawArrays(gl.LINES, 2 * (NUM_COLS + i), 2);
     }
 
-
+}
+function drawFunction() {
     gl.useProgram(program);
+    gl.uniform1i(funcLoc, document.getElementById("function").value);
+    gl.uniform1f(voltScaleLoc, document.getElementById("voltSlider").value * NUM_LINES);
+    gl.uniform1f(timeScaleLoc, document.getElementById("timeSlider").value * NUM_COLS);
+    gl.uniform1f(amplitudeLoc, document.getElementById("amplitude").value * NUM_LINES);
+    gl.uniform1f(timeLoc, time);
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.vertexAttribPointer(vTimeSample, 1, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vTimeSample);
 
 
     gl.drawArrays(gl.LINE_STRIP, 0, 9999);
+    time += 1 / (60 * 100);
+
+}
+
+
+function render() {
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    drawFunction();
+    drawGrid();
+
     requestAnimFrame(render);
 }
+
